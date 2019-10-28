7a2e676ce738ffe72d678c24815e39f4eafd6e43
Juan Carlos Jiménez Gutierrez <jcjimenez29@misena.edu.co>
2019-10-28 10:24:18 -0500

    [$] Yargs: Modulo de configuracion (comandos permitidos)

--- config/yargs.js
+++ config/yargs.js
@@ -5,16 +5,17 @@ const
         alias: 'd',      /** Crea un alias de la bandera establecida (en este caso una abreviación) */
         desc: 'Detalle de la nota de la tarea por hacer'
     },
+    completado = {       /** Nombre del flag (bandera) */
+        alias: 'c',      /** Crea un alias de la bandera establecida (en este caso una abreviación) */
+        default: true,   /** Valor por defecto en caso de no ser proporcionado */
+        desc: 'Estado de la tarea registrada en la nota'
+    },
     opt_crear = {
         descripcion      /** Configuración genérica */
     },
     opt_actualizar = {
         descripcion,        /** Configuración genérica */
-        completado: {       /** Nombre del flag (bandera) */
-            alias: 'c',     /** Crea un alias de la bandera establecida (en este caso una abreviación) */
-            default: true,  /** Valor por defecto en caso de no ser proporcionado */
-            desc: 'Estado de la tarea registrada en la nota'
-        }
+        completado
     },
     opt_eliminar = {
         descripcion        /** Configuración genérica */
